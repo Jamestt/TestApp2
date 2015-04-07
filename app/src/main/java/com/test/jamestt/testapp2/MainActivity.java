4fcbb06dc79efef1d6c69a169cddf77186a6ac9b
Jamestt <9a61c270@opayq.com>
2015-04-07 19:01:25 +0100

    Add Onclicklistener to button

--- app/src/main/java/com/test/jamestt/testapp2/MainActivity.java
+++ app/src/main/java/com/test/jamestt/testapp2/MainActivity.java
@@ -1,9 +1,12 @@
 package com.test.jamestt.testapp2;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,22 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Declare view variables
+        final TextView factLabel = (TextView) findViewById(R.id.factTextview);
+        Button showFactbutton = (Button) findViewById(R.id.showFactbutton);
+
+        //Create/set on click listener
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Update TextView when Button Clicked
+                String fact = "This is my new ant based fact";
+                factLabel.setText(fact);
+            }
+        };
+        showFactbutton.setOnClickListener(listener);
+
     }
 
 
